b289f114d306f6c13bac9a1c437029688cb0d474
austinTracy7 <101482688+austinTracy7@users.noreply.github.com>
2022-08-25 14:55:02 -0600

    Update salon.sh
    feat: finishing most of the user stories from freeCodeCamp. Testing will follow that they have been met.

--- salon.sh
+++ salon.sh
@@ -2,11 +2,8 @@
 
 PSQL='psql --csv --tuples-only --username=freecodecamp --dbname=salon -c'
 
-# declare -a Data
-
 ServicesData=$($PSQL "SELECT * FROM services;")
 
-# echo ${Data[0]}
 DISPLAY_SERVICES(){
 while IFS="," read -r ServiceId Name
 do
@@ -15,31 +12,63 @@ done << EOD
 $1
 EOD
 }
-DISPLAY_SERVICES "${ServicesData}"
-SELECTED_SERVICE=''
+
 TEST_FOR_SERVICE(){
 while IFS="," read -r ServiceId Name
 do
     if [ $ServiceId = $2 ]
     then
+      GLOBAL_SERVICE_ID_FOUND=0
       return 0
     else
-      return 1
+      GLOBAL_SERVICE_ID_FOUND=1
     fi
 done << EOD
 $1
 EOD
 }
-TEST_FOR_SERVICE "${ServicesData}" 1
-SERVICE_ID_FOUND=$(echo $?)
+
+GLOBAL_SERVICE_ID_FOUND=0
+SERVICE_ID_SELECTED=0
+
+GET_DESIRED_SERVICE(){
+  echo -e $1
+  DISPLAY_SERVICES "${ServicesData}"
+  read DESIRED_SERVICE
+  SERVICE_ID_SELECTED=$DESIRED_SERVICE
+  TEST_FOR_SERVICE "${ServicesData}" $DESIRED_SERVICE
+  if [ $GLOBAL_SERVICE_ID_FOUND = 1 ]
+  then
+    GET_DESIRED_SERVICE "\nThat isn't something we offer. Here's the list of things we offer again. Please pick the number corresponding to the service you desire."
+  fi
+
+}
+
+# SERVICE_ID_FOUND=$(echo $?)
 # # echo $Data
 echo $SERVICE_ID_FOUND
 
-# function myfunc()
-# {
-#     local  myresult='some value'
-#     echo "$myresult"
-# }
+# running the main script
+echo -e '\n~~~~~ Simply A Salon ~~~~~\n'
 
-# result=$(myfunc)   # or result=`myfunc`
-# echo $result
+GET_DESIRED_SERVICE "How may we be of service for you today?"
+
+SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED;")
+
+echo "Great, what is your phone number?"
+read CUSTOMER_PHONE
+
+CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE';")
+
+if [ -z CUSTOMER_NAME ]
+then
+  echo "What is your name?"
+  read CUSTOMER_NAME
+  INSERT_RESPONSE=$($PSQL "INSERT INTO customers(name,phone) values('$CUSTOMER_NAME','$CUSTOMER_PHONE');")
+fi
+
+echo "Okay. Just one more thing. When would you like to come by?"
+read SERVICE_TIME
+
+echo "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+
