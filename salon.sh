8066709a6ef94f1d4fd719c5d747a203e014390b
austinTracy7 <101482688+austinTracy7@users.noreply.github.com>
2022-08-25 15:18:29 -0600

    Update and rename better_implementation_but_not_passing_salon.sh to salon.sh
    fix: getting everything to pass and renaming as it didn't actually destroy the script

--- salon.sh
+++ salon.sh
@@ -34,9 +34,8 @@ SERVICE_ID_SELECTED=0
 GET_DESIRED_SERVICE(){
   echo -e $1
   DISPLAY_SERVICES "${ServicesData}"
-  read DESIRED_SERVICE
-  SERVICE_ID_SELECTED=$DESIRED_SERVICE
-  TEST_FOR_SERVICE "${ServicesData}" $DESIRED_SERVICE
+  read SERVICE_ID_SELECTED
+  TEST_FOR_SERVICE "${ServicesData}" $SERVICE_ID_SELECTED
   if [ $GLOBAL_SERVICE_ID_FOUND = 1 ]
   then
     GET_DESIRED_SERVICE "\nThat isn't something we offer. Here's the list of things we offer again. Please pick the number corresponding to the service you desire."
@@ -75,4 +74,3 @@ read SERVICE_TIME
 INSERT_RESPONSE=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) values($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME');")
 
 echo "I have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
-
